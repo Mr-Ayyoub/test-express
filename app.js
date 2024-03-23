1b37d161723e56020934fa425c42c0626098657e
Mr-Ayyoub <amhalayoub91@gmail.com>
2024-03-23 13:21:16 +0000

    Refactor

---
+++ app.js
@@ -0,0 +1,33 @@
+const express =  require('express');
+const  app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.get("/",(req, res)=>{
+    console.log("Get Request Successfull!");
+    res.send("get req successfully initiated");
+    });
+  
+app.delete("/delete", (req, res) => {
+    console.log("POST REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send(`Data Delete Request Recieved`);
+    })
+
+app.put("/put",(req, res)=> {
+    console.log("PUT request successful");
+    console.log(req.body);
+    res.send("Data Update Request Recueved");
+})
+
+app.put("/put",(req, res)=> {
+    console.log("PUT request successful");
+    console.log(req.body);
+    res.send("Data post Request Recueved");
+})
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server established at ${PORT}`);
+    });
